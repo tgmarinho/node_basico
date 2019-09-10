125e803f3c85f71a8175307598cb3837ad25fb70
Thiago Marinho <tgmarinho@gmail.com>
2019-09-10 11:55:57 -0400

    Aula 2 - Query e Route Params

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-// importo o módulo do express
 const express = require("express");
-// declaro uma porta para o servidor ouvir as requisições
+
 process.PORT = 3333;
-// inicio o servidor express
 const server = express();
-// declaro uma rota test que recebe uma requisição get e retorna um json com uma mensagem
-// abrir o navegador e acessar: http://localhost:3333:/test e vc vai ver a mensagem.
-server.get("/test", (req, res) => {
-  return res.json({ message: "Oi, eu sou um App feito com Node e Express!" });
+
+/**
+ * três tipos de parametros
+ * Query params = ?teste=1
+ * Route params = /users/1
+ * Request Body = { "name": "Thiago" }
+ */
+
+// Query params = ?teste=1
+// fazer a requisição no navegador: http://localhost:3333/teste/?nome=Thiago
+server.get("/teste", (req, res) => {
+  const nome = req.query.nome;
+  return res.json({ message: `Hello ${nome}` });
 });
+
+// Route params = /users/1
+// fazer a requisição no navegador: http://localhost:3333/users/1
+server.get("/users/:id", (req, res) => {
+  // const id = req.params.id;
+  const { id } = req.params; // desestruturando com ES06
+  return res.json({ message: `Buscando o usuário de ID: ${id}` });
+});
+
 // Inicia o servidor ouvindo a porta 3333
 server.listen(process.PORT, () => {
   console.log("executando o express na porta: " + process.PORT);
