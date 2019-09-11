3d83b6f99684053ebe353f4ed4983a37e69df017
Thiago Marinho <tgmarinho@gmail.com>
2019-09-11 11:44:13 -0400

    Aula 6 - Middlewares

--- index.js
+++ index.js
@@ -5,20 +5,46 @@ const server = express();
 
 server.use(express.json());
 
-/**
- * três tipos de parametros
- * Query params = ?teste=1
- * Route params = /users/1
- * Request Body = { "name": "Thiago" }
- */
-
 const users = ["Thiago", "Delacyr", "Filipe", "Pedro"];
 
-// Route params = /users/1
-// fazer a requisição no navegador: http://localhost:3333/users/1
-server.get("/users/:index", (req, res) => {
-  const { index } = req.params; // desestruturando com ES06
-  return res.json(users[index]);
+// Middleware global
+server.use((req, res, next) => {
+  console.time("A requisição foi chamada");
+  next();
+});
+
+server.use((req, res, next) => {
+  console.time("REQUEST");
+  console.log(`Método: ${req.method} 
+               na URL: ${req.url} `);
+  next();
+  console.timeEnd("REQUEST");
+});
+
+// Middleware Local específico
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "Name is required!" });
+  }
+
+  return next();
+}
+
+// Middleware Local específico
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+  if (!user) {
+    return res.status(400).json({ error: "User does not exists!" });
+  }
+
+  req.user = user;
+
+  return next();
+}
+
+// Lista usuário de index = ao que foi passado na rota
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  return res.json(req.user);
 });
 
 // Listar todos usuários
@@ -27,7 +53,7 @@ server.get("/users", (req, res) => {
 });
 
 // Criar usuário
-server.post("/users", (req, res) => {
+server.post("/users", checkUserExists, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -36,14 +62,14 @@ server.post("/users", (req, res) => {
 });
 
 // Editar/Atualizar usuário
-server.put("/users/:index", (req, res) => {
+server.put("/users/:index", checkUserInArray, checkUserExists, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
   users[index] = name;
   return res.json(users);
 });
 
-server.delete("/users/:index", (req, res) => {
+server.delete("/users/:index", checkUserInArray, (req, res) => {
   const { index } = req.params;
   users.splice(index, 1);
   return res.json({ message: "Deletado com sucesso" });
